534222701b1d55e49b1a62a108a0a8affe088b7d
lwbeamer <73843667+lwbeamer@users.noreply.github.com>
2021-09-11 22:03:32 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,139 @@
+// const submit = document.getElementById('submit-button')
+// const textBox = document.getElementById('x-textinput')
+//
+// function checkX(node){
+//     let content = node.value
+//     if (content.trim()) {
+//         if (isNaN(content)) {
+//             alert('X должен быть представлен числом!')
+//         }
+//     } else {
+//         alert('поле не может быть пустым!')
+//     }
+// }
+
+// submit.addEventListener('click', (event)=>{
+//     event.preventDefault()
+//     checkX(textBox)
+// })
+
+$(function (){
+
+    let numValueX = undefined
+    let numValueY= undefined
+    let numValueR = undefined
+
+    function isNumber(s){
+        if (s === 0) return true;
+        return (!isNaN(parseFloat(s)) && s);
+    }
+
+    function checkX(){
+        const X_MIN = -3;
+        const X_MAX = 3;
+        let value = $('#x-textinput').val();
+        let fieldValueX = value.trim();
+        fieldValueX = value.replace(',', '.');
+        numValueX = parseFloat(fieldValueX)
+        if (numValueX === +fieldValueX && isNumber(numValueX)){
+            if (numValueX>X_MIN && numValueX<X_MAX){
+                $('#x-textinput').removeClass('text-error')
+                return true;
+            } else{
+                $('#x-textinput').addClass('text-error')
+                return false;
+            }
+        } else{
+            $('#x-textinput').addClass('text-error')
+            return false;
+        }
+    }
+    function checkY(){
+        if (numValueY === undefined){
+            $('.y-buttons input').addClass('button-error')
+            $('.y-buttons input').removeClass('button-clicked')
+            return false;
+        } else {
+            $('.y-buttons input').removeClass('button-error')
+            // $('.y-buttons input').removeClass('button-clicked')
+            return true;
+        }
+    }
+
+    $('.y-buttons input').click(function (){
+       numValueY = $(this).val();
+       $('.y-buttons input').removeClass('button-clicked')
+       $(this).addClass('button-clicked')
+    });
+
+    $('.r-checkbox').click(function (){
+        let id = $(this).attr('id');
+        $('.r-checkbox').prop('checked',false)
+        $('#'+id).prop('checked',true)
+    });
+
+
+    function checkR(){
+        if ($('.r-checkbox').is(':checked')){
+            if ($('#r-checkbox1').is(':checked')) numValueR= $('#r-checkbox1').val();
+            if ($('#r-checkbox2').is(':checked')) numValueR= $('#r-checkbox2').val();
+            if ($('#r-checkbox3').is(':checked')) numValueR= $('#r-checkbox3').val();
+            if ($('#r-checkbox4').is(':checked')) numValueR= $('#r-checkbox4').val();
+            if ($('#r-checkbox5').is(':checked')) numValueR= $('#r-checkbox5').val();
+            $('.rbox-label').removeClass('checkbox-error')
+            return true;
+        } else {
+            $('.rbox-label').addClass('checkbox-error')
+            return false;
+        }
+    }
+
+    $('#input-form').on('submit', function(event){
+        event.preventDefault();
+        if(!(checkX() & checkY() & checkR())) return;
+        $.ajax({
+            url: 'main.php',
+            type: 'POST',
+            dataType: 'json',
+            data: {
+                x: numValueX,
+                r: numValueR,
+                y: numValueY,
+                time: new Date().getTimezoneOffset()
+            },
+            beforeSend: function () {
+                $('.sub-res-buttons input').attr('disabled', 'disabled');
+            },
+            success: function(data){
+                $('.sub-res-buttons input').attr('disabled', false);
+                if (data.isValid) {
+                let newRow = '<tr>';
+                newRow += '<td>' + data.x + '</td>>';
+                newRow += '<td>' + data.y + '</td>>';
+                newRow += '<td>' + data.r + '</td>>';
+                newRow += '<td>' + data.currentTime + '</td>>';
+                newRow += '<td>' + data.execTime + '</td>>';
+                newRow += '<td>' + data.result + '</td>>';
+                $('#result-content-table').append(newRow);
+                }
+            }
+        });
+    });
+
+    $('#input-form').on('reset', function(event){
+        numValueX = undefined;
+        numValueY = undefined;
+        numValueR = undefined;
+        $('#x-textinput').removeClass('text-error')
+        $('.y-buttons input').removeClass('button-error')
+        $('.rbox-label').removeClass('checkbox-error')
+        $('.y-buttons input').removeClass('button-clicked')
+    });
+
+
+});
+
+
+
+
+
---
+++ main.php
@@ -0,0 +1,83 @@
+<?php
+
+$xVal = $_POST['x'];
+$yVal = $_POST['y'];
+$rVal = $_POST['r'];
+$time = $_POST['time'];
+
+function checkX($xVal) {
+    $X_MIN = -3;
+    $X_MAX = 3;
+
+    if (!isset($xVal))
+        return false;
+
+    $numX = str_replace(',', '.', $xVal);
+    return is_numeric($numX) && $numX > $X_MIN && $numX < $X_MAX;
+}
+
+function checkY($yVal) {
+    $arrayY = array(-5,-4,-3,-2,-1,0,1,2,3);
+
+    if (!isset($yVal))
+        return false;
+
+    if (in_array($yVal, $arrayY)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function checkR($rVal) {
+    $arrayR = array(1,1.5,2,2.5,3);
+
+    if (!isset($rVal))
+        return false;
+
+    if (in_array($rVal, $arrayR)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function checkAll($xVal,$yVal,$rVal){
+    return checkX($xVal) && checkY($yVal) && checkR($rVal);
+}
+
+function hitRectangle($xVal, $yVal, $rVal){
+    return $xVal >= -$rVal/2 && $xVal <= 0 && $yVal <= $rVal && $yVal>=0;
+}
+
+function hitTriangle($xVal, $yVal, $rVal){
+    return $xVal >= 0 && $yVal >= 0 && $yVal <= $rVal/2 + $xVal;
+}
+
+function hitCircle($xVal, $yVal, $rVal){
+    return $xVal <= 0 && $yVal <= 0 && sqrt($xVal*$xVal + $yVal*$yVal) <= $rVal;
+}
+
+function isHit($xVal, $yVal, $rVal){
+    return hitRectangle($xVal, $yVal, $rVal) || hitTriangle($xVal, $yVal, $rVal) || hitCircle($xVal, $yVal, $rVal);
+}
+
+$isValid = checkAll($xVal, $yVal, $rVal);
+$isValidString = $isValid ? 'true' : 'false';
+$isHit = isHit($xVal, $yVal, $rVal);
+$isHitString = $isHit ? 'true' : 'false';
+
+$currentTime = date('H:i:s', time() - $time * 60);
+$execTime = round(microtime(true) - $_SERVER['REQUEST_TIME_FLOAT'], 7);
+
+$jsonData = '{' .
+    "\"isValid\":\"$isValidString\"," .
+    "\"x\":\"$xVal\"," .
+    "\"y\":\"$yVal\"," .
+    "\"r\":\"$rVal\"," .
+    "\"currentTime\":\"$currentTime\"," .
+    "\"execTime\":\"$execTime\"," .
+    "\"result\":$isHitString" .
+    "}";
+
+echo $jsonData;
